0ce61c453793db749954aa4edbe747e80912453a
Om Prakash <Om.Prakash@ingenico.com>
2022-09-27 13:44:14 +0200

    Correcting assertions

--- src/test/java/net/sympower/cityzen/apx/ApxDataLoaderRefactoredTest.java
+++ src/test/java/net/sympower/cityzen/apx/ApxDataLoaderRefactoredTest.java
@@ -1,7 +1,6 @@
 package net.sympower.cityzen.apx;
 
 import net.sympower.cityzen.apxrefactored.model.Quote;
-import net.sympower.cityzen.apxrefactored.model.QuoteValue;
 import net.sympower.cityzen.apxrefactored.util.ApxDataLoaderRefactored;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
@@ -9,8 +8,8 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 ;
 
@@ -26,14 +25,11 @@ public class ApxDataLoaderRefactoredTest {
 
         List<Quote> response = apxDataLoader.load();
 
-        assertNotNull(response.equals(null));
-        assertNotNull(response.isEmpty());
-        assertTrue(response.size() > 0);
-
+        assertNotNull(response);
+        assertFalse(response.isEmpty());
         Quote quoteFields = response.get(0);
-        QuoteValue quoteValueFields = response.get(0).getQuoteValues().get(0);
-        assertNotNull(quoteFields.equals(null));
-        assertNotNull(quoteValueFields.equals(null));
+        assertNotNull(quoteFields.getQuoteValues());
+        assertNotNull(quoteFields);
 
     }
 
